02d123df13086b11543d355072e2914782a27bb1
Alex Pasquali <alexpasquali98@gmail.com>
2022-08-16 16:26:31 +0200

    Added departure time to NSPRs

--- src/environments/network_simulator.py
+++ src/environments/network_simulator.py
@@ -19,7 +19,8 @@ class NetworkSimulator(gym.Env):
     def __init__(self, psn_file: str, nsprs_path: str):
         """ Constructor
         :param psn_file: GraphML file containing the definition of the PSN
-        :param nsprs_path: either directory with the GraphML files defining the NSPRs or path to a single GraphML file
+        :param nsprs_path: either directory with the GraphML files defining the
+            NSPRs or path to a single GraphML file
         """
         super(NetworkSimulator, self).__init__()
 
@@ -38,7 +39,8 @@ class NetworkSimulator(gym.Env):
         self._map_id_idx = {nodes_ids[idx]: idx for idx in range(len(nodes_ids))}
 
         # map (dict) between an index of a list (incrementing int) and the ID of a server
-        servers_ids = [node_id for node_id, node in self.psn.nodes.items() if node['NodeType'] == 'server']
+        servers_ids = [node_id for node_id, node in self.psn.nodes.items()
+                       if node['NodeType'] == 'server']
         self._servers_map_idx_id = {idx: servers_ids[idx] for idx in range(len(servers_ids))}
 
         # partial rewards to be accumulated across the steps of evaluation of a single NSPR
@@ -127,7 +129,7 @@ class NetworkSimulator(gym.Env):
         self._load_balancing_rewards = []
 
     @staticmethod
-    def enough_available_resources(physical_node: dict, vnf: dict) -> bool:
+    def enough_avail_resources(physical_node: dict, vnf: dict) -> bool:
         """ Check that the physical node has enough resources to satisfy the VNF's requirements
 
         :param physical_node: physical node to check
@@ -145,6 +147,26 @@ class NetworkSimulator(gym.Env):
                 self.cur_nspr = self.waiting_nsprs.pop(0)
                 self.cur_nspr_unplaced_vnfs_ids = list(self.cur_nspr.nodes.keys())
                 self.cur_vnf_id = self.cur_nspr_unplaced_vnfs_ids.pop(0)
+
+    def check_for_departed_nsprs(self):
+        """ Checks it some NSPRs have reached their departure time and in case
+        it frees the PSN resources occupied by them. """
+        all_arrival_times = list(self.nsprs.keys())
+        all_arrival_times.sort()
+        for arrival_time in all_arrival_times:
+            if arrival_time >= self.time_step:
+                break
+            cur_nspr = self.nsprs[arrival_time]
+            for nspr in cur_nspr:
+                departed = nspr.graph.get('departed', False)
+                if nspr.graph['DepartureTime'] <= self.time_step and not departed:
+                    self.restore_avail_resources(nspr=nspr)
+                    if nspr == self.cur_nspr:
+                        # haven't finished placing this NSPR, but its departure time has come.
+                        # remove NSPR, no reward, neither positive nor negative
+                        # (not agent's fault, too many requests at the same time)
+                        self.cur_nspr = None
+                        self.reset_partial_rewards()
 
     def manage_unsuccessful_action(self) -> Tuple[GymObs, int]:
         """ Method to manage an unsuccessful action, executed when a VNF/VL cannot be placed onto the PSN.
@@ -184,8 +206,9 @@ class NetworkSimulator(gym.Env):
         """ Perform an action in the environment
 
         :param action: the action to be performed
-            more in detail, it's the index in the list of server corresponding ot a certain server ID,
-            the mapping between this index and the server ID is done in the self._servers_map_idx_id dictionary
+            more in detail, it's the index in the list of server corresponding
+            ot a certain server ID, the mapping between this index and the
+            server ID is done in the self._servers_map_idx_id dictionary
         :return: next observation, reward, done (True if the episode is over), info
         """
         reward, done, info = 0, False, {}
@@ -196,6 +219,7 @@ class NetworkSimulator(gym.Env):
         #     obs, reward = self.manage_unsuccessful_action()
         #     return obs, reward, done, info
 
+        self.check_for_departed_nsprs()
         self.waiting_nsprs += self.nsprs.get(self.time_step, [])
         self.pick_next_nspr()
 
@@ -204,7 +228,7 @@ class NetworkSimulator(gym.Env):
             physical_node_id = self._servers_map_idx_id[action]
             physical_node = self.psn.nodes[physical_node_id]
 
-            if not self.enough_available_resources(physical_node=physical_node, vnf=self.cur_vnf):
+            if not self.enough_avail_resources(physical_node=physical_node, vnf=self.cur_vnf):
                 # the VNF cannot be placed on the physical node
                 obs, reward = self.manage_unsuccessful_action()
                 return obs, reward, done, info
@@ -221,7 +245,7 @@ class NetworkSimulator(gym.Env):
             )
 
             # connect the placed VNF to the other VNFs it's supposed to be connected to
-            cur_vnf_VLs = self.get_cur_vnf_vls(self.cur_vnf_id, self.cur_nspr)  # get the VLs involving the current VNF
+            cur_vnf_VLs = self.get_cur_vnf_vls(self.cur_vnf_id, self.cur_nspr)
             if not cur_vnf_VLs:
                 # if the VNF is detached from all others, R.C. reward is 1,
                 # so it's the neutral when aggregating the rewards into the global one
@@ -235,15 +259,22 @@ class NetworkSimulator(gym.Env):
                     # if the VL isn't placed yet and both the source and target VNFs are placed, place the VL
                     if not vl['placed'] and source_node >= 0 and target_node >= 0:
                         self._cur_vl_reqBW = vl['reqBW']
-                        psn_path = nx.shortest_path(G=self.psn, source=source_node, target=target_node,
-                                                    weight=self.compute_link_weight, method='dijkstra')
+                        psn_path = nx.shortest_path(G=self.psn,
+                                                    source=source_node,
+                                                    target=target_node,
+                                                    weight=self.compute_link_weight,
+                                                    method='dijkstra')
 
-                        """ if NO path is available, 'nx.shortest_path' will return an invalid path.
-                        Only after the whole VL has been placed, it is possible to restore the resources availabilities,
-                        so we use this variable to save that the resources have been exceeded as soon as we find this to happen,
-                        and only after the VL placement, if this var is True, we restore the resources availabilities. """
+                        """ if NO path is available, 'nx.shortest_path' will
+                        return an invalid path. Only after the whole VL has been
+                        placed, it is possible to restore the resources
+                        availabilities, so we use this variable to save that the
+                        resources have been exceeded as soon as we find this to
+                        happen, and only after the VL placement, if this var is
+                        True, we restore the resources availabilities. """
                         exceeded_bw = False
-                        # place the VL onto the PSN and update the resources availabilities of the physical links involved
+                        # place VL onto the PSN
+                        # and update the resources availabilities of physical links involved
                         for i in range(len(psn_path) - 1):
                             physical_link = self.psn.edges[psn_path[i], psn_path[i + 1]]
                             physical_link['availBW'] -= vl['reqBW']
@@ -257,7 +288,8 @@ class NetworkSimulator(gym.Env):
 
                         # update the resource consumption reward
                         path_length = len(psn_path) - 1
-                        self._cur_resource_consumption_rewards.append(1 / path_length if path_length > 0 else 1)
+                        self._cur_resource_consumption_rewards.append(
+                            1 / path_length if path_length > 0 else 1)
 
                 # aggregate the resource consumption rewards into a single value for this action
                 n_VLs_placed_now = len(self._cur_resource_consumption_rewards)
@@ -319,15 +351,14 @@ class NetworkSimulator(gym.Env):
         return 1 if link['availBW'] >= self._cur_vl_reqBW else math.inf
 
     def restore_avail_resources(self, nspr: nx.Graph):
-        """ Method called in case a NSPR is not accepted.
-        Restores the resources if the PSN that had been already allocated for the rejected NSPR
+        """ Method called in case a NSPR is not accepted, or it has reached
+        its departure time.
+        Restores the PSN resources occupied by that NSPR.
 
         :param nspr: the rejected NSPR
         """
         if nspr is not None:
-            """ Why this if-statement is needed?
-            If this method is called, self.cur_nspr can be None only in case the environment has been
-            wrapped to prevent infeasible actions, the selected action is -1 and we finished the NSPRs. """
+            nspr.graph['departed'] = True
             for vnf_id, vnf in nspr.nodes.items():
                 # restore nodes' resources availabilities
                 if vnf['placed'] >= 0:
@@ -337,7 +368,7 @@ class NetworkSimulator(gym.Env):
             for _, vl in nspr.edges.items():
                 # restore links' resources availabilities
                 if vl['placed']:
-                    # if vl['placed'] is not empty, it's the list of the physical nodes traversed by the link
+                    # vl['placed'] is the list of the physical nodes traversed by the link
                     for i in range(len(vl['placed']) - 1):
                         physical_link = self.psn.edges[vl['placed'][i], vl['placed'][i + 1]]
                         physical_link['availBW'] += vl['reqBW']
--- src/reader.py
+++ src/reader.py
@@ -4,7 +4,7 @@ from typing import Tuple, List, Dict
 import networkx as nx
 
 
-def _check_if_graphml(file: str):
+def check_if_graphml(file: str):
     """ Checks if a file is a GraphML file (checking the extension)
 
     :param file: path to the file to be checked
@@ -14,20 +14,13 @@ def _check_if_graphml(file: str):
         raise ValueError("{} is not a GraphML file".format(file))
 
 
-def _check_required_attributes(network: nx.Graph, required_node_attributes: Tuple[str, ...],
-                               required_link_attributes: Tuple[str, ...], **admissible_values: tuple):
-    """ Checks whether all the required attributes are present in the nodes and link of the network passed as argument
+def _check_graph(network: nx.Graph):
+    """ Checks that the graph is correct
 
-    :param network: network whose nodes and links have to be checked (if they contain all the required attributes)
-    :param required_node_attributes: tuple containing all the required attributes for the network's nodes
-    :param required_link_attributes: tuple containing all the required attributes for the network's links
-    :param admissible_values: (optional) extra arguments where the name is an attribute name and the value is a tuple with the admissible values
+    :param network: network that needs to be checked
 
-    :raise AssertionError:
-        - in case some nodes/links don't contain all the required parameters
-        - in case some non-admissible values are used for some arguments
+    :raise AssertionError: if some graph's attributes are not correct
     """
-    # check graph
     if "E2ELatency" in network.graph.keys():
         assert network.graph['E2ELatency'] > 0
         # if E2ELatency is present, it means the network is a NSPR
@@ -35,20 +28,38 @@ def _check_required_attributes(network: nx.Graph, required_node_attributes: Tupl
             assert network.graph['ArrivalTime'] >= 0
         else:
             network.graph['ArrivalTime'] = 0
+        if "DepartureTime" in network.graph.keys():
+            assert network.graph['DepartureTime'] >= \
+                   network.graph['ArrivalTime'] + len(network.nodes.keys())
 
-    # check nodes
+
+def _check_nodes(network: nx.Graph, required_node_attributes: Tuple[str, ...],
+                 **admissible_values: tuple):
+    """ Checks that the nodes of the network are correct
+
+    :param network: network whose nodes have to be checked
+    :param required_node_attributes: tuple with all required attributes for the nodes
+    :param admissible_values: (optional) extra arguments where the name is an
+        attribute name and the value is a tuple with the admissible values
+
+    :raise AssertionError:
+        - in case some nodes don't contain all the required parameters
+        - in case some non-admissible values are used for some arguments
+    """
     for node_id, node in network.nodes.items():
-        # check that - if the admissible values for a certain attribute are passed - the value of each attribute is admissible
+        # if the admissible values for a certain attribute are passed,
+        # check that the value of each attribute is admissible
         for attrib, value in node.items():
             assert value in admissible_values.get(attrib, (value,))
             if attrib in ("CPUcap", "RAMcap", "availCPU", "availRAM", "reqCPU", "reqRAM"):
                 assert value >= 0
         # the following checks are for servers or VNFs only, in case skip
-        if node.get("NodeType", "server") != "server":  # if the node hasn't attribute "NodeType", it's a VNF, so don't skip iteration
+        if node.get("NodeType", "server") != "server":
+            # if node hasn't attrib "NodeType", it's a VNF, so don't skip iteration
             continue
-        # add an attribute to specify if a VNF has been placed onto the PSN (initialized as False)
         if "reqCPU" in node.keys():
             # 'reqCPU' is a mandatory argument for NSPR, so if it's present, the node is a VNF
+            # add an attribute to specify if a VNF has been placed onto the PSN
             node['placed'] = -1
         else:
             # it means the node belongs to a PSN and not to a NSPR
@@ -57,14 +68,27 @@ def _check_required_attributes(network: nx.Graph, required_node_attributes: Tupl
         # check that all required attributes are present in the current node
         assert all(req_attrib in node.keys() for req_attrib in required_node_attributes)
 
-    # check edges
+
+def _check_edges(network: nx.Graph, required_link_attributes: Tuple[str, ...], **admissible_values: tuple):
+    """ Checks that the edges of the network are correct
+
+    :param network: network whose edges have to be checked
+    :param required_link_attributes: tuple with all required attributes for the links
+    :param admissible_values: (optional) extra arguments where the name is an
+        attribute name and the value is a tuple with the admissible values
+
+    :raise AssertionError:
+        - in case some links don't contain all the required parameters
+        - in case some non-admissible values are used for some arguments
+    """
     for node_A, node_B in list(network.edges):
         cur_link_attribs = network.edges[node_A, node_B].keys()
         cur_link_values = network.edges[node_A, node_B].values()
         cur_link_attribs_values = zip(cur_link_attribs, cur_link_values)
         # check that all required attributes are present in the current link
         assert all(attrib in cur_link_attribs for attrib in required_link_attributes)
-        # check that - if the admissible values for a certain attribute are passed - the value of each attribute is admissible
+        # if the admissible values for a certain attribute are passed,
+        # check that the value of each attribute is admissible
         for attrib, value in cur_link_attribs_values:
             assert value in admissible_values.get(attrib, (value,))
             if attrib in ("BWcap", "reqBW", "Latency", "reqLatency"):
@@ -78,6 +102,25 @@ def _check_required_attributes(network: nx.Graph, required_node_attributes: Tupl
             network.edges[node_A, node_B]['availBW'] = network.edges[node_A, node_B]['BWcap']
 
 
+def check_required_attributes(network: nx.Graph, required_node_attributes: Tuple[str, ...],
+                              required_link_attributes: Tuple[str, ...], **admissible_values: tuple):
+    """ Checks whether all the required attributes are present in the nodes and link of the network passed as argument
+
+    :param network: network whose nodes and links have to be checked
+    :param required_node_attributes: tuple with all required attributes for the nodes
+    :param required_link_attributes: tuple with all required attributes for the links
+    :param admissible_values: (optional) extra arguments where the name is an
+        attribute name and the value is a tuple with the admissible values
+
+    :raise AssertionError:
+        - in case some nodes/links don't contain all the required parameters
+        - in case some non-admissible values are used for some arguments
+    """
+    _check_graph(network)
+    _check_nodes(network, required_node_attributes, **admissible_values)
+    _check_edges(network, required_link_attributes, **admissible_values)
+
+
 def read_psn(graphml_file: str) -> nx.Graph:
     """ Reads a GraphML file containing the definition of a PSN
 
@@ -87,16 +130,16 @@ def read_psn(graphml_file: str) -> nx.Graph:
     :raise ValueError: if "graphml_file" is not a GraphML file
     :raise AssertionError: if some required attributes of nodes and links are missing
     """
-    _check_if_graphml(graphml_file)  # check if the file passed is a GraphML file
+    check_if_graphml(graphml_file)  # check if the file passed is a GraphML file
 
     # read the GraphML file and create a nx.Graph object
     psn = nx.read_graphml(path=graphml_file, node_type=int)
 
     # check that the attributes of the graph are correct
-    _check_required_attributes(network=psn,
-                               required_node_attributes=("NodeType", "CPUcap", "RAMcap"),
-                               required_link_attributes=("BWcap",),
-                               NodeType=("UAP", "router", "switch", "server"))
+    check_required_attributes(network=psn,
+                              required_node_attributes=("NodeType", "CPUcap", "RAMcap"),
+                              required_link_attributes=("BWcap",),
+                              NodeType=("UAP", "router", "switch", "server"))
     return psn
 
 
@@ -109,23 +152,25 @@ def read_single_nspr(graphml_file: str) -> nx.Graph:
     :raise ValueError: if "graphml_file" is not a GraphML file
     :raise AssertionError: if some required attributes of nodes and links are missing
     """
-    _check_if_graphml(graphml_file)  # check if the file passed is a GraphML file
+    check_if_graphml(graphml_file)  # check if the file passed is a GraphML file
 
     # read the GraphML file and create a nx.Graph object
     nspr = nx.read_graphml(path=graphml_file, node_type=int)
 
     # check that the attributes of the graph are correct
-    _check_required_attributes(network=nspr,
-                               required_node_attributes=("reqCPU", "reqRAM"),
-                               required_link_attributes=("reqBW",))
+    check_required_attributes(network=nspr,
+                              required_node_attributes=("reqCPU", "reqRAM"),
+                              required_link_attributes=("reqBW",))
     return nspr
 
 
 def read_nsprs(nsprs_path: str) -> Dict[int, List[nx.Graph]]:
     """ Reads all the NSPRs (network slice placement requests) in a directory
 
-    :param nsprs_path: either path to the directory with the files defining a NSPR each or the path to a single NSPR
-    :return: a dict having as keys the arrival times of the NSPRs and as values the NSPRs themselves
+    :param nsprs_path: either path to the directory with the files defining a
+        NSPR each or the path to a single NSPR
+    :return: a dict having as keys the arrival times of the NSPRs and as
+        values the NSPRs themselves
     :raise ValueError: if nsprs_path is neither a directory nor a file
     """
     if not os.path.isdir(nsprs_path) and not os.path.isfile(nsprs_path):
