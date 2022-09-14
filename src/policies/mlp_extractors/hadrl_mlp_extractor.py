Removed softmax from within the mlp_extractor + corrected minor bug in simulator
The softmax should be added automatically by the library when creating a distribution over actions starting from the logits, i.e. the output of the mlp_extractor (in teoria...)

src/policies/mlp_extractors/hadrl_mlp_extractor.py
@@ -19,9 +19,11 @@ class HADRLActor(nn.Module):
             nn.Linear(in_features=n_nodes * gcn_out_channels + nspr_out_features,
                       out_features=n_nodes),
 
+            nn.Tanh(),
+
             # TODO: put the heuristics here
 
-            nn.Softmax(dim=1)
+            # nn.Softmax(dim=1)
         )
 
     def forward(self, x):