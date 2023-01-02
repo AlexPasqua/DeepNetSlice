8618f558ac5960b308a4a9000cdd0ba0fa08930c
AlexPasqua <a.pasquali5@studenti.unipi.it>
2023-01-02 15:57:29 +0000

    DFS connectivity first, then random
    Problema (+ o -): c'è una linea di collegamenti che passa da tutti i nodi, in fila.
    Prossimamente: aggiungi link randomicamente partendo da un unvisited node e collegandolo ad un altro casuale. Poi metti visited quel nodo (o quei due nodi)

--- src/wrappers/dynamic_connectivity.py
+++ src/wrappers/dynamic_connectivity.py
@@ -10,28 +10,62 @@ class DynamicConnectivity(gym.Wrapper):
         self.link_bw = link_bw
         self.psn = self.unwrapped.psn
         self.tot_bw_cap = sum([edge['BWcap'] for edge in self.psn.edges.values()])
+        self.placed_bw = 0
     
     def reset(self, **kwargs):
-        self.psn = self.remove_all_edges()
-        self.psn = self.add_edges()
+        # remove all edges from the PSN
+        self.remove_all_edges()
+        # initialize the bandwidth placed in the PSN
+        self.placed_bw = 0
+        # add edges in the PSN until the target bandwidth capacity is reached
+        self.add_edges()
+        # TODO: check if needed -> se usa puntatori in teoria non serve
+        self.unwrapped.psn = self.psn
     
     def remove_all_edges(self):
         for (u, v), edge in self.psn.edges.items():
             self.psn.remove_edge(u, v)
 
     def add_edges(self):
-        raise NotImplementedError
-        # # start connecting with DFS, so no node is isolated
-        # unvisited = set(self.psn.nodes)
-        # start = random.choice(unvisited)
-        # visited = set(start)
-        # unvisited.remove(start)
-        # stack = [start]
-        # while stack:
-        #     current = stack[-1]
-        #     unvisited_neighbors = [n for n in self.psn.neighbors(current) if n in unvisited]
-        #     if not unvisited_neighbors:
-        #         stack.pop()
-        #     else:
-        #         neighbor = random.choice(unvisited_neighbors)
-        #         self.psn.add_edge(current, neighbor)
+        """Add edges to the PSN
+
+        Start connecting via DFS, to ensure that no node is isolated from the rest.
+        Then, if the target bandwidth is not reached, reach it adding random links.
+        """
+        # set of unvisited nodes
+        unvisited = set(self.psn.nodes)
+        # starting node
+        start = random.choice(list(self.psn.nodes))
+        # set of visited nodes
+        visited = {start}
+        # remove 'start' from unvisited nodes
+        unvisited.remove(start)
+        # stack to hold the nodes in the current path
+        stack = [start]
+        while stack:
+            # current node
+            current = stack[-1]
+            # If there are no unvisited nodes, backtrack
+            if not unvisited:
+                stack.pop()
+            else:
+                # choose a random unvisited node
+                neighbor = random.choice(list(unvisited))
+                if neighbor != current:
+                    # connect the current node with the chosen neighbor
+                    self.psn.add_edge(current, neighbor, BWcap=self.link_bw, availBW=self.link_bw)
+                    # save the amount of bandwidth introduced in the PSN
+                    self.placed_bw += self.link_bw
+                    # add neighbor to the set of visited nodes
+                    visited.add(neighbor)
+                    # remove neighbor from the set of unvisited nodes
+                    unvisited.remove(neighbor)
+                    # push the neighbor onto the stack (next current node)
+                    stack.append(neighbor)
+
+        # if the total bandwidth of the PSN hasn't been reached, reach it by adding random links
+        while self.placed_bw < self.tot_bw_cap:
+            u, v = random.sample(self.psn.nodes, 2)
+            bw = min(self.link_bw, self.tot_bw_cap - self.placed_bw)
+            self.psn.add_edge(u, v, BWcap=bw, availBW=bw)
+            self.placed_bw += bw
